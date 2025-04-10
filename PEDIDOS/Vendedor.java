6f859aeecbd2442e8c7c415c7d5a4d4c551e2b6a
new-ceub <newlandsp@sempreceub.com>
2025-04-10 10:53:16 -0300

    Add files via upload

---
+++ PEDIDOS/Vendedor.java
@@ -0,0 +1,8 @@
+public class Vendedor {
+    private String nameVendedor;
+    private int registrationVendedor;
+    public Vendedor (String nameVendedor, int registrationVendedor){
+        this.nameVendedor = nameVendedor;
+        this.registrationVendedor = registrationVendedor;
+    }
+}
